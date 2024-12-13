72fc75fde09ed9a3c3a1b89628c41848357ca59d
Hamza Touhardji <h.touhardji@gmail.com>
2024-12-13 16:36:51 +0100

    chore(style): add prettier syntaxe

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,10 +1,8 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react';
 
 const DashboardCard: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md max-w-[80vw]">
-      {children}
-    </div>
+    <div className="bg-white rounded-lg shadow-md max-w-[80vw]">{children}</div>
   );
 };
 
